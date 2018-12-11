f480f5ca8ae5e3bce40ee1fff233b324bf391421
d.beltran <bleachstick@hotmail.com>
2018-12-11 06:49:44 -0500

    first commit

---
+++ src/main/java/edu/eci/arsw/securedapp/SpringbootAuthUpdatedApplication.java
@@ -0,0 +1,12 @@
+package edu.eci.arsw.securedapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAuthUpdatedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAuthUpdatedApplication.class, args);
+	}
+}
